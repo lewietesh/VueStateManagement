56754d1ee915cdd97bedbce0afa7e009c11fe910
mutembei-lewis <lewismutembei001@gmail.com>
2022-05-26 23:18:37 +0300

    vuex commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,80 +2,42 @@
   <div class="home container">
     <div class="row">
       <div class="col-md-9 pt-5">
-
-       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-3">
-          <Product v-for="product in products" :product="product" :key="product.id" />
-       </div>
-        
+        <div
+          class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-3 row-cols-xl-3"
+        >
+          <Product
+            v-for="product in products"
+            :product="product"
+            :key="product.id"
+          />
+        </div>
       </div>
       <div class="col-md-3 pt-5">
-        <!-- <SideNav /> -->
         <Cart />
       </div>
-      
     </div>
   </div>
 </template>
 
 <script>
-// import SideNav from "../components/SideNav.vue";
 import Product from "../components/Product.vue";
 import Cart from "../components/Cart.vue";
 
+import { mapState } from "vuex";
+
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    //SideNav,
     Product,
     Cart
   },
-  data(){
-    return {
-      products : [
-        {
-          id : 1,
-          name : "Chelsea Shoes",
-          price : 200,
-          shortdesc : "Best Drip in the Market",
-          url : "images/chelsea-shoes.png"
-        },
-        {
-          id : 2,
-          name : "Kimono",
-          price : 50,
-          shortdesc : "Classy, Stylish, Dope",
-          url : "images/kimono.png"
-        },
-        {
-          id : 3,
-          name : "Rolex",
-          price : 2500,
-          shortdesc : "Elegance built in",
-          url : "images/rolex.png"
-        },
-        {
-          id : 4,
-          name : "Baelerry Wallet",
-          price : 80,
-          shortdesc : "Sleek, Trendy, Clean",
-          url : "images/wallet.png"
-        },
-        {
-          id : 5,
-          name : "Lady Handbags",
-          price : 230,
-          shortdesc : "Fabulous, Exotic, Classy",
-          url : "images/handbag.png"
-        },
-        {
-          id : 6,
-          name : "Casual Shirts",
-          price : 30,
-          shortdesc : "Neat, Sleek, Smart",
-          url : "images/shirt.png"
-        }
-      ]
-    }
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState({
+      products: (state) => state.products
+    })
   }
-}
+};
 </script>
