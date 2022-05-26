472a9df3b91912edb69383f5c5787f63b8f77173
mutembei-lewis <lewismutembei001@gmail.com>
2022-05-26 22:45:24 +0300

    Initial commit. Vue State Management Tutorial

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <router-link to="/" class="navbar-brand">The Identity Store</router-link>
+
+        
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon" />
+        </button>
+        
+      </nav>
+      
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
